f78e05f4bfbe3b6152b2dcea04777b038c79df79
MihaelaBoian <pinzari_m@mail.ru>
2023-11-20 14:45:29 +0200

    M

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Employee {
+import java.util.* ;
+//import java.util.stream;
+public class Employee {
+    String name;
+    String Department;
+    boolean isPayedPerHour;
+    int age;
+    public Employee(String name, String Department,boolean isPayedPerHour,int age ){
+        this.name=name;
+        this.age=age;
+        this.isPayedPerHour=isPayedPerHour;
+        this.Department=Department;
+    }
+
+    public String getDepartment() {
+        return Department;
+    }
+
+    public boolean isPayedPerHour() {
+        return isPayedPerHour;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String  name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "Name: " + name + " Age: " + age + " Department: " + Department;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+       List<Employee> employees = new ArrayList<>();
+            employees.add(new Employee("Tom", "Management", true, 19));
+            employees.add(new Employee("Mike", "Management", false, 27));
+            employees.add(new Employee("Julia", "Software", true, 30));
+            employees.add(new Employee("Mike","Testing",true,35));
+            employees.add(new Employee("Robert","Management",true,38));
+        List<Employee>employeefilter = employees.stream()
+                 .filter(e -> e.isPayedPerHour())
+                 .filter(e -> e.getDepartment().equals("Management"))
+                 .collect(Collectors.toList());
+         System.out.println(employeefilter);
+
+
+
     }
 }
